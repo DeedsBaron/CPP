43bfb95d01ee22de30dd0edd0ad8a1d970475ee1
dbaron <grimmjoki@gmail.com>
2021-08-06 17:45:42 +0300

    in progress

--- Day02/ex03/Point.cpp
+++ Day02/ex03/Point.cpp
@@ -1,12 +1,9 @@
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-Point::Point() : x(0), y(0) {}
+Point::Point(void) : x(0), y(0) {}
 
-Point::Point(const Point& ins)
-{
-	*this = ins;
-}
+Point::Point(const Point& ins) : x(ins.x), y(ins.y) {}
 
 Point::Point(const float x, const float y) : x(x), y(y) {}
 
@@ -16,7 +13,8 @@ Point&			Point::operator=(const Point& right)
 {
 	if (this == &right)
 		return (*this);
-	return ()
+	else
+		return (*this);
 }
 
 const Fixed&			Point::get_x(void) const {return (this->x);}
@@ -24,6 +22,6 @@ const Fixed&			Point::get_y(void) const {return (this->y);}
 
 std::ostream& operator<<(std::ostream& os, const Point& obj)
 {
-	os << "x = " << obj.get_x() << "\t y = " << obj.get_y() << std::endl;
+	os << "x = " << obj.get_x() << "\t y = " << obj.get_y();
 	return (os);
 }
--- Day02/ex03/main.cpp
+++ Day02/ex03/main.cpp
@@ -2,10 +2,10 @@
 #include "Point.hpp"
 int main(void) {
 
-	Point p1(10.56f, 20.23f);
-	std::cout << p1;
-	Point p2(0, 0);
-	p1 = p2;
-	std::cout << p1;
+	Point p1;
+	std::cout << p1 << std::endl;
+	Point p2(10.56f, 20.23f);
+	std::cout << p2 << std::endl;
+	std::cout << p2 << std::endl;
 	return 0;
 }
